a77d0938358ea290f426c4442152acaa4c6fcc8b
Syduta <thibault.touzin@gmail.com>
2023-10-18 16:39:59 +0200

    mongo-db container added

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
+const mongoose = require('mongoose');
+const { MONGO_USERNAME, MONGO_PASSWORD, MONGO_IP, MONGO_PORT } = require('./config/config');
+
+const connectWithRetry = () => {
+    mongoose.connect(`mongodb://${MONGO_USERNAME}:${MONGO_PASSWORD}@${MONGO_IP}:${MONGO_PORT}/?authSource=admin`)
+    .then(() => console.log("(^w^) connecté à la base de données (^w^)"))
+    .catch((err) => {
+        console.log(err)
+        setTimeout(connectWithRetry, 5000)
+    });
+}
+
+connectWithRetry();
 
 app.get("/", (req,res) => {
     res.send("<h2>Hello There !</h2>");
 });
 
-app.listen(port, () => console.log(`listening on port ${port}`));
+app.listen(port, () => console.log(`(^w^) connecté au port ${port} (^w^)`));
