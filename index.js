c6b63d959fe0c283d1ee7e5154d5571865e1beab
Syduta <thibault.touzin@gmail.com>
2023-10-19 11:04:05 +0200

    basic crud app

--- index.js
+++ index.js
@@ -3,20 +3,24 @@ const app = express();
 const port = process.env.PORT || 3000;
 const mongoose = require('mongoose');
 const { MONGO_USERNAME, MONGO_PASSWORD, MONGO_IP, MONGO_PORT } = require('./config/config');
+const postRouter = require("./routes/postRoutes");
 
 const connectWithRetry = () => {
     mongoose.connect(`mongodb://${MONGO_USERNAME}:${MONGO_PASSWORD}@${MONGO_IP}:${MONGO_PORT}/?authSource=admin`)
-    .then(() => console.log("(^w^) connecté à la base de données (^w^)"))
-    .catch((err) => {
-        console.log(err)
-        setTimeout(connectWithRetry, 5000)
-    });
+        .then(() => console.log("(^w^) connecté à la base de données (^w^)"))
+        .catch((err) => {
+            console.log(err)
+            setTimeout(connectWithRetry, 5000)
+        });
 }
 
 connectWithRetry();
+app.use(express.json());
 
-app.get("/", (req,res) => {
+app.get("/", (req, res) => {
     res.send("<h2>Hello There !</h2>");
 });
+
+app.use("/api/v1/posts", postRouter);
 
 app.listen(port, () => console.log(`(^w^) connecté au port ${port} (^w^)`));
